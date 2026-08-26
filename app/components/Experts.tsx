ea4474d9ae392d2e32a03f7c2027ef97f48d9529
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2026-08-26 10:23:30 +0100

    Fixed broken image displayg Experts

--- app/components/Experts.tsx
+++ app/components/Experts.tsx
@@ -26,7 +26,7 @@ const experts = [
     name: "Isac Carpie",
     position: "Cybersecurity Expert",
     badge: "Expert",
-    image: "/Expert6.jpg",
+    image: "/worker3.jpg",
     bio: "Tracks and analyses encrypted transaction trails to identify threat actors.",
   },
   {
